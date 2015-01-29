ffdc9c4adc6d3b97f0bf81a47c4227212deb4ddf
Chrysanthi Vandera <vandera.chryssanthi@gmail.com>
2015-01-29 01:49:15 -0500

    hw3 added html form in calculator (front-end)

--- hw3/calculator/views.py
+++ hw3/calculator/views.py
@@ -2,4 +2,9 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return render(request, 'calculator/calculator.html', {})
+#     give back initial variable values
+
+    return render(request, 'calculator/calculator.html', {"last_operation": '', "last_screen": '0', "last_result": '0', "operation": 'false'})
+
+# def calculate(request):
+#     int 
