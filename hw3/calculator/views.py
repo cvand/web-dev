a468579d69f1bb996a3ac2db26f39e7fb6f8c785
Chrysanthi Vandera <vandera.chryssanthi@gmail.com>
2015-01-30 02:11:06 -0500

    hw3 added protection against malformed input

--- hw3/calculator/views.py
+++ hw3/calculator/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 
+
 # Create your views here.
 def index(request):
 #     give back initial variable values
